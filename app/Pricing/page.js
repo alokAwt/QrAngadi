a3dbd5e460c307f9021e3365fb1870c8597cd28a
vedavyas@angadiworldtech.com <vedavyas@angadiworldtech.com>
2024-02-22 17:36:35 +0530

    added gamification component to pricing page

--- app/Pricing/page.js
+++ app/Pricing/page.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+// import Navbar from "../../components/navbar/Navbar";
+// import PricingCards from "../../components/pricingCards/PricingCards";
+// import Faqs from "../../components/faq";
+import GamificationAddOns from "../../Components/GamificationAddOns/index";
 
-const page = () => {
+function Pricing() {
   return (
-    <div>Pricing</div>
-  )
+    <>
+      <div className="mt-10 text-center">
+        <p className="font-bold  text-4xl md:text-5xl lg:text-6xl mb-4">
+          Choose the right plan for you!
+        </p>
+        <p className="font-medium text-xl md:text-2xl">
+          Choose a plan and enjoy an exceptional user experience instantly.
+        </p>
+      </div>
+      {/* <PricingCards />
+      <Faqs /> */}
+      <GamificationAddOns />
+    </>
+  );
 }
 
-export default page
+export default Pricing;
