112f7b1879a2bbc82290dabc76689ef53fb0070a
alok-56 <alokkumar11746@gmail.com>
2024-03-02 15:30:48 +0530

    pull code

--- Components/Homecomponents/Imageslider.js
+++ Components/Homecomponents/Imageslider.js
@@ -6,7 +6,7 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel";
+} from "/components/ui/carousel";
 import Slider from "../../public/Slider2.png";
 import Slider1 from "../../public/Slider1.png";
 import Image from "next/image";
