3aba789f3a38724eee1fce0c92fa36e1e6bf9553
Santoshalimkar <santosh.sa4d@gmail.com>
2024-03-05 19:59:35 +0530

    Changes added

--- app/Profile/layout.js
+++ app/Profile/layout.js
@@ -1,18 +1,38 @@
-'use client'
 
-import NextUIprovider from "@/components/NextUIprovider"
+"use client";
+import ProfileNav from "@/components/ProfileComponents/ProfileNav";
+import Sidebar from "@/components/ProfileComponents/Sidebar";
+import React, { useEffect } from "react";
+import { UseStatevalue } from "@/Utility/Contextfiles/StateProvider";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+const Layout = ({ children }) => {
+  const [{token}]=UseStatevalue()
+  const pathname=usePathname()
+  const router = useRouter();
 
 
+  useEffect(() => {
+    const protectedRoutes = ['/Profile', '/Analytics', '/gamification','/QRcodesolution'];
+    if (protectedRoutes.includes(pathname) || pathname.startsWith('/Analytics')) {
+      if (token===null) {
+        router.replace('/Login');
+      }
+    }
+  }, [pathname, token]);
 
-const layout = ({ children }) => {
-  
-
-
-
- 
   return (
-   <NextUIprovider children={children} />
+    <main className='flex sticky top-0'>
+    <div className="">
+      <Sidebar/>
+    </div>
+    <div className="flex flex-col w-full">
+      <ProfileNav/>
+      {children}
+    </div>
+  </main>
   )
 }
 
-export default layout
+export default Layout
