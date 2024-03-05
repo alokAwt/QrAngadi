ab3339b09a97cf2cefa4eeb00fc94fe8bcbbaa7d
Santoshalimkar <santosh.sa4d@gmail.com>
2024-03-05 20:12:32 +0530

    changes added

--- app/Profile/layout.js
+++ app/Profile/layout.js
@@ -1,4 +1,3 @@
-
 "use client";
 import ProfileNav from "@/components/ProfileComponents/ProfileNav";
 import Sidebar from "@/components/ProfileComponents/Sidebar";
@@ -8,31 +7,38 @@ import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 
 const Layout = ({ children }) => {
-  const [{token}]=UseStatevalue()
-  const pathname=usePathname()
+  const [{ token }] = UseStatevalue();
+  const pathname = usePathname();
   const router = useRouter();
 
-
   useEffect(() => {
-    const protectedRoutes = ['/Profile', '/Analytics', '/gamification','/QRcodesolution'];
-    if (protectedRoutes.includes(pathname) || pathname.startsWith('/Analytics')) {
-      if (token===null) {
-        router.replace('/Login');
+    const protectedRoutes = [
+      "/Profile",
+      "/Analytics",
+      "/gamification",
+      "/QRcodesolution",
+    ];
+    if (
+      protectedRoutes.includes(pathname) ||
+      pathname.startsWith("/Analytics")
+    ) {
+      if (token === null) {
+        router.replace("/Login");
       }
     }
   }, [pathname, token]);
 
   return (
-    <main className='flex sticky top-0'>
-    <div className="">
-      <Sidebar/>
-    </div>
-    <div className="flex flex-col w-full">
-      <ProfileNav/>
-      {children}
-    </div>
-  </main>
-  )
-}
+    <main className="flex sticky top-0">
+      <div className="">
+        <Sidebar />
+      </div>
+      <div className="flex flex-col w-full">
+        <ProfileNav />
+        {children}
+      </div>
+    </main>
+  );
+};
 
-export default Layout
+export default Layout;
