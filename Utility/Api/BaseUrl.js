1aef592c8382607050e6a64280a0130c96122c0d
AJ <147396185+alokAwt@users.noreply.github.com>
2024-03-13 13:26:08 +0530

    Update BaseUrl.js

--- Utility/Api/BaseUrl.js
+++ Utility/Api/BaseUrl.js
@@ -1,2 +1,2 @@
-// export const BaseUrl = "https://qrangadi.qrangadi.com/api/v1";
-export const BaseUrl = "http://localhost:9100/api/v1"
+export const BaseUrl = "https://qrangadi.qrangadi.com/api/v1"
+
