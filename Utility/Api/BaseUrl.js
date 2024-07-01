96933a19eb06ba6b0a0e47dc4b1c91ce13bed8f0
Santoshalimkar <santosh.sa4d@gmail.com>
2024-07-01 18:09:51 +0530

    Changes added

--- Utility/Api/BaseUrl.js
+++ Utility/Api/BaseUrl.js
@@ -1,4 +1,5 @@
-export const BaseUrl = "https://qrangadi.qrangadi.com/api/v1";
+export const BaseUrl = "http://195.250.20.204:9100/api/v1";
+// export const BaseUrl = "https://qrangadi.qrangadi.com/api/v1";
 // export const BaseUrl = "http://192.168.225.113:9100/api/v1";
 // export const BaseUrl = "http://localhost:9100/api/v1";
 
